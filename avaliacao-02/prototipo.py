21774d187cbbe9ab5149acd3a94b39f2d186dfda
Seu Nome <seu@email.com>
2024-09-20 10:50:48 -0300

    modificando e testando prototipo:

--- avaliacao-02/prototipo.py
+++ avaliacao-02/prototipo.py
@@ -1,4 +1,8 @@
 import pywhatkit as kit
+import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import time
 
 # Função para ler o arquivo
@@ -8,10 +12,24 @@ def ler_arquivo(caminho_arquivo):
     return [linha.strip() for linha in linhas]
 
 # Função para enviar mensagem via WhatsApp
+def abrirZap():
+    caminho = "https://web.whatsapp.com/"
+    webbrowser.open(caminho);
+    
+driver = webdriver.Chrome(executable_path='.\\chrome.exe')
 def enviar_mensagem(contato, mensagem):
-    # Envia a mensagem (agendada para 20 segundos após a execução)
-    kit.sendwhatmsg_instantly(f"+{contato}", mensagem)
-    time.sleep(20)  # Aguarda para evitar o bloqueio por múltiplas mensagens
+      # Busca o campo de pesquisa de contato
+    search_box = driver.find_element("xpath", '//div[@contenteditable="true"][@data-tab="3"]')
+    search_box.clear()
+    search_box.send_keys(contato)
+    time.sleep(2)
+    search_box.send_keys(Keys.ENTER)
+
+    # Localiza a caixa de texto para enviar a mensagem
+    chat_box = driver.find_element("xpath", '//div[@contenteditable="true"][@data-tab="6"]')
+    chat_box.send_keys(mensagem)
+    chat_box.send_keys(Keys.ENTER)
+    time.sleep(2)  # Tempo para evitar problemas de envio
 
 # Caminho para o arquivo de texto
 caminho_arquivo = ".\\teste.txt"
@@ -20,6 +38,8 @@ contato_whatsapp = "5548920005078"
 
 # Lê o arquivo e envia cada linha como uma mensagem
 linhas = ler_arquivo(caminho_arquivo)
+
+abrirZap();
 
 for linha in linhas:
     if linha:  # Verifica se a linha não está vazia
